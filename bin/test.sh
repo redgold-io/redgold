1a0145c57f1aad66e304fe3c571f92653e154e36
Redgold <112849952+redgold-labs@users.noreply.github.com>
2023-05-26 17:05:40 -0700

    Release issues? Or cache? (#96)

--- bin/test.sh
+++ bin/test.sh
@@ -1,3 +1,2 @@
-export RUST_MIN_STACK=10485760
 # TODO: Remove --test-threads when extraneous test parallelism is fixed
-cargo test --lib -- --test-threads=1
+export RUST_MIN_STACK=10485760; cargo test --lib -- --test-threads=1
