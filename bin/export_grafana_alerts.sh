af4e7460db2abdbe35a0e09d54c6e0178d5f5aec
Redgold <112849952+redgold-labs@users.noreply.github.com>
2023-06-18 10:32:15 -0700

    Partial setup for alert rules for grafana (#224)
    Hasn't been fully tested yet with an email hook. Also fix a deploy issue
    with genesis flag #213

---
+++ bin/export_grafana_alerts.sh
@@ -0,0 +1,3 @@
+
+curl -H "Authorization: Bearer $1" "http://$2:3000/api/v1/provisioning/alert-rules/$3"
+#curl -X POST -H 'Content-Type: application/json' -H "Authorization: Bearer $1" -H 'X-Disable-Provenance: true' -d @alerts.json http://$2:3000/api/v1/provisioning/alert-rules
