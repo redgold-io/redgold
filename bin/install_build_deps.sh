3cd01a7269c38836c1ddcc57eb9060be923b22ba
Redgold <112849952+redgold-labs@users.noreply.github.com>
2023-08-07 00:14:52 -0700

    Switch to stable, comment extism, lto=true and drop optimization to 1 (#322)
    Fixes current dev issue

--- bin/install_build_deps.sh
+++ bin/install_build_deps.sh
@@ -4,4 +4,4 @@ sudo apt install -y libdigest-sha3-perl
 # GUI dependencies on linux, not really necessary except for bundled UI
 sudo apt-get install -y libxcb-xfixes0-dev libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev
 
-rustup default nightly
+#rustup default nightly
