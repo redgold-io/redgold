65ae14b198dbc75a79ed44b22ca087f538cf8c17
Redgold <info@redgold.io>
2023-08-12 23:21:05 -0700

    build.rs debugging

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ use std::process::Command;
 fn main() {
     println!("Build script started");
     let o = Command::new("bash")
-        .args("./sdk/compile.sh")
+        .args(vec!["./sdk/compile.sh"])
         .output()
         .expect("SDK compile failure");
     // println!("Build sdk compile output stdout: {}", )
