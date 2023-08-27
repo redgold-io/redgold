7378ded7e5e87b8f13f78b5f55b3ebd537227dd0
Redgold <info@redgold.io>
2023-08-26 20:08:36 -0700

    Build ubuntu 20

--- bin/copy_usb.sh
+++ bin/copy_usb.sh
@@ -3,9 +3,17 @@ wget https://github.com/redgold-io/redgold/releases/download/release%2Fdev/redgo
 -O redgold
 chmod +x redgold
 # TODO: Pick up usb drive with bash?
-cp redgold /Volumes/Samsung\ USB/redgold
+#cp redgold /Volumes/Samsung\ USB/redgold
+#cp redgold /Volumes/NO\ NAME/redgold
 
-
+# Check if the first directory exists and copy there, otherwise check the second directory
+if [ -d "/Volumes/Samsung USB" ]; then
+    cp redgold "/Volumes/Samsung USB/redgold"
+elif [ -d "/Volumes/NO NAME" ]; then
+    cp redgold "/Volumes/NO NAME/redgold"
+else
+    echo "Neither directory exists!"
+fi
 
 
 
