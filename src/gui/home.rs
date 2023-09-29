b05a5a79b0a5d397a2d649b22d92fdd024417bde
Redgold <info@redgold.io>
2023-09-28 18:35:27 -0700

    Add staging query to ui

--- src/gui/home.rs
+++ src/gui/home.rs
@@ -15,8 +15,8 @@ use crate::node_config::NodeConfig;
 use crate::util;
 
 pub fn gui_status_networks() -> Vec<NetworkEnvironment> {
-    // let mut vec = NetworkEnvironment::status_networks();
-    vec![NetworkEnvironment::Dev]
+    let mut vec = NetworkEnvironment::status_networks();
+    vec![NetworkEnvironment::Dev, NetworkEnvironment::Staging]
     // vec
 }
 
