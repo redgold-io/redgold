3cc426d38d7284a8b3ae825cda1cdeef5e00f37e
Redgold <info@redgold.io>
2023-11-15 21:52:21 -0800

    dependencies

--- bin/install_build_deps.sh
+++ bin/install_build_deps.sh
@@ -6,7 +6,7 @@ sudo apt install -y libdigest-sha3-perl
 #openpnp capture
 sudo apt install -y nasm
 # Not required on all ubuntu images, but some servers need this to build
-sudo apt-get install -y llvm libclang-dev cmake
+sudo apt-get install -y llvm libclang-dev cmake libjpeg-turbo8-dev #libjpeg is for openpnp-capture
 # GUI dependencies on linux, not really necessary except for bundled UI
 sudo apt-get install -y libxcb-xfixes0-dev libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev
 
