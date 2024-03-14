887d8785e9bb7e073a3393f3d6a131d97aed2da3
tesla-aether <Tesla@yoyo.com>
2024-03-14 06:12:31 -0700

    Git diffs and issues crawler

---
+++ ai/src/github_info.rs
@@ -0,0 +1,42 @@
+use octocrab::{models, params};
+use octocrab::models::issues::Issue;
+use redgold_schema::{ErrorInfoContext, RgResult};
+
+pub async fn issues() -> RgResult<Vec<Issue>> {
+    let octocrab = octocrab::instance();
+// Returns the first page of all issues.
+    let mut page = octocrab
+        .issues("redgold-io", "redgold")
+        .list()
+        // Optional Parameters
+        // .creator("XAMPPRocky")
+        .state(params::State::All)
+        .per_page(100)
+        .send()
+        .await
+        .error_info("Failed to get issues")?;
+
+    let mut all = vec![];
+// Go through every page of issues. Warning: There's no rate limiting so
+// be careful.
+    loop {
+        for issue in &page {
+            println!("{} {}", issue.number, issue.title);
+            all.push(issue.clone());
+        }
+        page = match octocrab
+            .get_page::<Issue>(&page.next)
+            .await.error_info("Failed to get next page")?
+        {
+            Some(next_page) => next_page,
+            None => break,
+        }
+    }
+    Ok(all)
+}
+
+#[ignore]
+#[tokio::test]
+async fn test_issues() {
+    issues().await.unwrap();
+}
--- ai/src/lib.rs
+++ ai/src/lib.rs
@@ -1,3 +1,5 @@
 mod directory_code_reader;
 pub mod lsp_analyze;
 pub mod embed;
+pub mod git_diffs;
+pub mod github_info;
