9913b72978d1be1a2fa4339b4b2e811a5abd5eb7
Sergei <sergei@redgold.io>
2024-03-24 17:48:43 -0700

    Table sizes / v1 API routes

--- data/src/data_store.rs
+++ data/src/data_store.rs
@@ -6,7 +6,7 @@ use std::sync::Arc;
 use itertools::Itertools;
 use log::info;
 use metrics::gauge;
-use sqlx::SqlitePool;
+use sqlx::{Row, SqlitePool};
 use sqlx::sqlite::{SqliteConnectOptions, SqliteJournalMode};
 
 use crate::address_block::AddressBlockStore;
@@ -42,6 +42,43 @@ pub struct DataStore {
 }
 
 impl DataStore {
+
+    pub async fn table_size(&self, table_name: impl Into<String>) -> RgResult<Vec<i64>> {
+        let string = table_name.into();
+        let string1 = format!(r#"SELECT sum("pgsize") as aggregate FROM dbstat WHERE name='{}'"#, string.as_str());
+        let rows = DataStoreContext::map_err_sqlx(sqlx::query(
+            // r#"select aggregate from dbstat('main',1) where name='utxo'"#,
+            string1.as_str(),
+
+        )
+            .fetch_all(&mut *self.ctx.pool().await?)
+            .await)?;
+        let mut res = vec![];
+        for r in rows {
+            // let name: String = DataStoreContext::map_err_sqlx(r.try_get("name"))?;
+            let aggregate: i64 = DataStoreContext::map_err_sqlx(r.try_get("aggregate"))?;
+            res.push(aggregate);
+        }
+        Ok(res)
+    }
+
+
+    pub async fn table_sizes(&self) -> RgResult<Vec<(String,i64)>> {
+        let rows = DataStoreContext::map_err_sqlx(sqlx::query(
+            r#"SELECT name, sum("pgsize") as aggregate FROM dbstat GROUP BY name"#
+        )
+            .fetch_all(&mut *self.ctx.pool().await?)
+            .await)?;
+        let mut res = vec![];
+        for r in rows {
+            let name: String = DataStoreContext::map_err_sqlx(r.try_get("name"))?;
+            let aggregate: i64 = DataStoreContext::map_err_sqlx(r.try_get("aggregate"))?;
+            res.push((name, aggregate));
+        }
+        Ok(res)
+    }
+
+
     pub async fn resolve_code(&self, address: &Address) -> RgResult<structs::ResolveCodeResponse> {
         let res = self.utxo.code_utxo(address, true).await?;
         let mut resp = structs::ResolveCodeResponse::default();
