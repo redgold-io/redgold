a6db49879f0abfdd819aeb97adf2f4be651ea2c2
Redgold <info@redgold.io>
2024-08-02 12:40:17 -0700

    better to switch to python at this point for claude code

--- ai/src/claude_dbg.rs
+++ ai/src/claude_dbg.rs
@@ -1,27 +1,50 @@
+use llmclient::claude::{call_claude_completion, ClaudeCompletion};
+use llmclient::gpt::GptMessage as ClaudeMessage;
 
+// Switch to python for now
 
-
+#[ignore]
 #[tokio::test]
 async fn debug() {
     let mut messages = Vec::new();
 
+    // let input = "~/self/ai-prompts/self.txt";
+    // let input = "/Volumes/self/ai-prompts/self.txt";
+
     let user_role = "user";
     let ast_role = "assistant";
 
-    let ast_msg =
-            messages.push(ClaudeMessage { role: role.into(), content: c.to_string() });
-        });
+    let system = r#"
+    You are an AI agent.
+    Your job is to receive chat messages from a user and then respond to them and do things.
+    This prompt is just to test if you're able to call functions.
+    "#;
 
+    let model = "claude-3-5-sonnet-20240620";
+    let temperature = 1.0;
+
+    let test_user_msg = "What is the current weather like? \
+    Please use your function tool to answer";
+
+    messages.push(ClaudeMessage { role: user_role.into(), content: test_user_msg.to_string() });
+
+    let mut functions = vec![];
+
+    let function = Some(functions);
     let completion = ClaudeCompletion {
         model: model.into(),
         tools: function,
-        system: if system.is_empty() { None } else { Some(system.to_string()) },
+        system: Some(system.into()),
         messages,
         temperature,
         max_tokens: 4096
     };
 
-    call_claude_completion(&completion).await
+    let result = call_claude_completion(&completion).await;
+
+    let ret = result.expect("test");
+
+    println!("{:?}", ret);
 
 
 }
