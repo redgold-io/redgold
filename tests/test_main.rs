815dca57d0c8130bd2de16b18bceab1ba0b6310e
Redgold <info@redgold.io>
2024-09-29 18:02:07 -0700

    Remove test

--- tests/test_main.rs
+++ tests/test_main.rs
@@ -1,18 +1,18 @@
-
-use std::thread::sleep;
-use std::time::Duration;
-use redgold::core::run_main::main_from_args;
-use redgold_schema::conf::rg_args::RgArgs;
-use redgold::util::runtimes::{build_simple_runtime};
-
-#[test]
-fn run_main() {
-
-    // TODO: Change the runtime structure to implement the shutdowns directly inside, then pass
-    // around the Arc reference externally for spawning.
-    let rt = build_simple_runtime(1, "test_run_main");
-    let args = RgArgs::default();
-    rt.spawn_blocking(move || main_from_args(args));
-    sleep(Duration::from_secs(10));
-    rt.shutdown_timeout(Duration::from_secs(10));
-}
+//
+// use std::thread::sleep;
+// use std::time::Duration;
+// use redgold::core::run_main::main_from_args;
+// use redgold_schema::conf::rg_args::RgArgs;
+// use redgold::util::runtimes::{build_simple_runtime};
+//
+// #[test]
+// fn run_main() {
+//
+//     // TODO: Change the runtime structure to implement the shutdowns directly inside, then pass
+//     // around the Arc reference externally for spawning.
+//     let rt = build_simple_runtime(1, "test_run_main");
+//     let args = RgArgs::default();
+//     rt.spawn_blocking(move || main_from_args(args));
+//     sleep(Duration::from_secs(10));
+//     rt.shutdown_timeout(Duration::from_secs(10));
+// }
