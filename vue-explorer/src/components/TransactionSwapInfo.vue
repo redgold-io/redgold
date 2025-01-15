55db2505d735c86d042fab92337882be31473fd0
Redgold <info@redgold.io>
2025-01-14 18:56:40 -0800

    Pre change

--- vue-explorer/src/components/TransactionSwapInfo.vue
+++ vue-explorer/src/components/TransactionSwapInfo.vue
@@ -8,42 +8,74 @@
       </tr>
       </thead>
       <tbody>
-      <tr><td>Swap Status</td></tr>
-      <tr><td>{{ swapInfo.swap_status }}</td></tr>
-      <tr><td>Input Currency</td> </tr>
-      <tr><td>{{ swapInfo.input_currency }}</td> </tr>
-      <tr><td>Input Amount</td> </tr>
-      <tr><td>{{ swapInfo.swap_input_amount }}</td> </tr>
-      <tr><td>Input USD</td> </tr>
-      <tr><td>{{ swapInfo.swap_input_amount_usd }}</td> </tr>
-      <tr><td>Input USD Now</td> </tr>
-      <tr><td>{{ swapInfo.swap_input_amount_usd_now }}</td> </tr>
-      <tr><td>Output Currency</td> </tr>
-      <tr><td>{{ swapInfo.output_currency }}</td> </tr>
-      <tr><td>Output Amount</td> </tr>
-      <tr><td>{{ swapInfo.swap_output_amount }}</td> </tr>
-      <tr><td>Output USD</td> </tr>
-      <tr><td>{{ swapInfo.swap_output_amount_usd }}</td> </tr>
-      <tr><td>Output USD Now</td> </tr>
-      <tr><td>{{ swapInfo.swap_output_amount_usd_now }}</td> </tr>
-      <tr><td>Exchange Rate</td> </tr>
-      <tr><td>{{ swapInfo.swap_exchange_rate }}</td> </tr>
-      <tr><td>Party Address</td> </tr>
-      <tr><td>{{ swapInfo.party_address }}</td> </tr>
-      <tr><td>Swap Destination</td> </tr>
-      <tr><td>{{ swapInfo.exchange_rate }}</td> </tr>
-      <tr><td>Is Request</td> </tr>
-      <tr><td>{{ swapInfo.is_request }}</td> </tr>
-      <tr><td>Is Fulfillment</td> </tr>
-      <tr><td>{{ swapInfo.is_fulfillment }}</td> </tr>
-      <tr><td>Request TX Hash</td> </tr>
-      <tr><td>
-        <HashLink v-if="swapInfo.request_tx_hash" :data="swapInfo.request_tx_hash" />
-      </td> </tr>
-      <tr><td>Fulfillment TX Hash</td></tr>
-      <tr><td>
-        <HashLink v-if="swapInfo.fulfillment_tx_hash" :data="swapInfo.fulfillment_tx_hash" />
-      </td> </tr>
+      <tr>
+        <td>Swap Status</td>
+        <td>{{ swapInfo.swap_status }}</td>
+      </tr>
+      <tr>
+        <td>Input Currency</td>
+        <td>{{ swapInfo.input_currency }}</td>
+      </tr>
+      <tr>
+        <td>Input Amount</td>
+        <td>{{ swapInfo.swap_input_amount }}</td>
+      </tr>
+      <tr>
+        <td>Input USD</td>
+        <td>{{ swapInfo.swap_input_amount_usd }}</td>
+      </tr>
+      <tr>
+        <td>Input USD Now</td>
+        <td>{{ swapInfo.swap_input_amount_usd_now }}</td>
+      </tr>
+      <tr>
+        <td>Output Currency</td>
+        <td>{{ swapInfo.output_currency }}</td>
+      </tr>
+      <tr>
+        <td>Output Amount</td>
+        <td>{{ swapInfo.swap_output_amount }}</td>
+      </tr>
+      <tr>
+        <td>Output USD</td>
+        <td>{{ swapInfo.swap_output_amount_usd }}</td>
+      </tr>
+      <tr>
+        <td>Output USD Now</td>
+        <td>{{ swapInfo.swap_output_amount_usd_now }}</td>
+      </tr>
+      <tr>
+        <td>Exchange Rate</td>
+        <td>{{ swapInfo.swap_exchange_rate }}</td>
+      </tr>
+      <tr>
+        <td>Party Address</td>
+        <td>{{ swapInfo.party_address }}</td>
+      </tr>
+      <tr>
+        <td>Swap Destination</td>
+        <td>{{ swapInfo.exchange_rate }}</td>
+      </tr>
+      <tr>
+        <td>Is Request</td>
+        <td>{{ swapInfo.is_request }}</td>
+      </tr>
+      <tr>
+        <td>Is Fulfillment</td>
+        <td>{{ swapInfo.is_fulfillment }}</td>
+      </tr>
+      <tr>
+        <td>Request TX Hash</td>
+        <td>
+          <HashLink v-if="swapInfo.request_tx_hash" :data="swapInfo.request_tx_hash" />
+        </td>
+      </tr>
+      <tr>
+        <td>Fulfillment TX Hash</td>
+        <td>
+          <HashLink v-if="swapInfo.fulfillment_tx_hash" :data="swapInfo.fulfillment_tx_hash" />
+        </td>
+      </tr>
       </tbody>
     </table>
   </div>
@@ -99,5 +131,27 @@ th {
   background-color: #191a19 !important;
   color: #ffffff !important;
 }
+.table-container {
+  width: auto;
+  min-width: 300px;
+  max-width: fit-content;
+}
+
+.table {
+  width: auto;
+  table-layout: fixed;
+}
+
+.table td:first-child {
+  white-space: nowrap;
+  padding-right: 2rem;
+}
+
+.table td:last-child {
+  min-width: 200px;
+  max-width: 400px;
+  word-break: break-all;
+}
+
 
 </style>
